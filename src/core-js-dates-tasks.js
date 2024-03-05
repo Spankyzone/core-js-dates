b7ed17e8f655ead925f1015bf12b9fee9ae58bd6
Aliaksandr <144029953+Spankyzone@users.noreply.github.com>
2024-03-06 01:21:40 +0300

    feat: impl 9 first tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return new Date(date).getTime();
 }
 
 /**
@@ -31,8 +31,9 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const options = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+  return new Intl.DateTimeFormat('ru-RU', options).format(date);
 }
 
 /**
@@ -46,8 +47,9 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const myDate = new Date(Date.parse(date));
+  return myDate.toLocaleString('en-US', { weekday: 'long' });
 }
 
 /**
@@ -61,8 +63,13 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const currentDate = new Date(date);
+  let daysToAdd = 5 - currentDate.getDay();
+  if (daysToAdd <= 0) {
+    daysToAdd += 7;
+  }
+  return new Date(currentDate.setDate(currentDate.getDate() + daysToAdd));
 }
 
 /**
@@ -76,8 +83,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -91,8 +98,13 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const millisecondsInDay = 1000 * 60 * 60 * 24;
+  return (
+    (new Date(dateEnd).getTime() - new Date(dateStart).getTime()) /
+      millisecondsInDay +
+    1
+  );
 }
 
 /**
@@ -112,8 +124,14 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateToCheck = new Date(date).getTime();
+  const start = new Date(period.start).getTime();
+  const end = new Date(period.end).getTime();
+  if (start <= dateToCheck && dateToCheck <= end) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -127,8 +145,10 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Date(date).toLocaleString('en-US', {
+    timeZone: 'UTC',
+  });
 }
 
 /**
@@ -143,8 +163,20 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const firstDayOfMonth = new Date(year, month - 1, 1);
+  const lastDayOfMonth = new Date(year, month, 0);
+  let weekendDays = 0;
+  for (
+    let date = firstDayOfMonth;
+    date <= lastDayOfMonth;
+    date.setDate(date.getDate() + 1)
+  ) {
+    if ([0, 6].includes(date.getDay())) {
+      weekendDays += 1;
+    }
+  }
+  return weekendDays;
 }
 
 /**
